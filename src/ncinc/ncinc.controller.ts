56be01a62bbff412b807c0fb4dbbcce1947adb6a
ihlmpy <jsih1236@gmail.com>
2025-02-28 20:30:01 +0100

    Ncinc Controller changed at querys in request for get cevil data

--- src/ncinc/ncinc.controller.ts
+++ src/ncinc/ncinc.controller.ts
@@ -31,15 +31,17 @@ export class NcincController {
 
     @Get("get-civil")
     @UseGuards(AuthGuard, RolesGuard)
-    async getCivil(@Query('name') name?: string, @Query('idcard') idCard?: string) {
+    async getCivil(@Query('name') name?: string, @Query('idcard') idCard?: string, @Query("id") id?: number) {
         let civil: Civil[] = [];
         if (name) {
             civil = await this.prisma.civil.findMany({ where: { name }, include: civilInclude });
         } else if (idCard) {
             civil = await this.prisma.civil.findMany({ where: { idCard }, include: civilInclude });
+        } else if (id) {
+            civil = await this.prisma.civil.findMany({ where: { id: Number(id) }, include: civilInclude });
         } else {
             throw new HttpException("Not allowed param for this request", 404);
-        }
+        } 
 
         if (civil.length > 0) {
             return civil;
@@ -50,11 +52,13 @@ export class NcincController {
 
     @Get("get-auto")
     @UseGuards(AuthGuard, RolesGuard)
-    async getAuto(@Query("plate") plate?: string) {
+    async getAuto(@Query("plate") plate?: string, @Query("id") id?: number) {
         let auto: Car[] = [];
 
         if (plate) {
             auto = await this.prisma.car.findMany({ where: { plate }, include: autoInclude });
+        } else if (id) {
+            auto = await this.prisma.car.findMany({ where: { id: Number(id) }, include: autoInclude });
         } else {
             throw new HttpException("Not allowed param for this request", 404);
         }
@@ -68,11 +72,13 @@ export class NcincController {
 
     @Get("get-weapon")
     @UseGuards(AuthGuard, RolesGuard)
-    async getWeapon(@Query("serial") serial: string) {
+    async getWeapon(@Query("serial") serial: string, @Query("id") id: number) {
         let weapon: Weapon[] = [];
 
         if (serial) {
             weapon = await this.prisma.weapon.findMany({ where: { serial }, include: autoInclude });
+        } else if (id) {
+            weapon = await this.prisma.weapon.findMany({ where: { id: Number(id) }, include: autoInclude })
         } else {
             throw new HttpException("Not allowed param for this request", 404);
         }
